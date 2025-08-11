b638a36d05d8eaa342c9cf98fe4f0a747a41353a
Chris Atwood <76616367+chrisatwoodbmx@users.noreply.github.com>
2025-08-11 23:45:58 +0100

    force deploy

--- lib/db.ts
+++ lib/db.ts
@@ -16,7 +16,6 @@ export async function hashPassword(password: string): Promise<string> {
   const saltRounds = 12;
   return await bcrypt.hash(password, saltRounds);
 }
-
 export async function verifyPassword(
   password: string,
   hashedPassword: string,
